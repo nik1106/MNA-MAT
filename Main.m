b68c0ca86c225dcde7ad06ffca0551cb3ba9141b
nik1106 <nik110695@gmail.com>
2016-08-28 00:21:43 +0530

    Add files via upload

---
+++ Main.m
@@ -0,0 +1,25 @@
+clc;
+clear;
+close all;
+flag = input('<< Welcome to MNA-MAT >> \n 1. Standard Analysis \n 2. MonteCarlo \n 3. View the result of previous simulation \n');
+switch(flag)
+    case{1}
+        run('Standard_Analysis.m');
+    case{2}
+        run('MonteCarlo.m');
+    case{3}
+        if(exist('Results.txt'))
+            type('Results.txt');
+        elseif(exist('Results.xls'))
+            open('Results.xls');
+            if(exist('MC_values_ode.txt'))
+                type('MC_values_ode.txt');
+            end
+            open('Voltages_graph.fig');
+            if(exist('Currents_graph.fig'))
+                open('Currents_graph.fig');
+            end
+        else
+            disp('No files of previous simulation found');
+        end
+end
